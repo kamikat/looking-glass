b460645441e3150b8867d65414b647b2ff24acad
kamikat <kamikat@banana.moe>
2018-03-13 02:01:29 +0800

    change method naming

--- lg/winapp.py
+++ lg/winapp.py
@@ -17,26 +17,30 @@ class OverlayWindow(BaseWindow):
         BaseWindow.__init__(self)
 
     @subscribe
-    def onmessage(self, hwnd, message, wparam, lparam):
+    def on_message(self, hwnd, message, wparam, lparam):
         print("[%d] message=%-24s wparam=0x%-10x lparam=0x%-10x" % (hwnd, get_message_name(message), wparam, lparam))
 
     @subscribe(WM_CREATE)
-    def oncreate(self, hwnd, message, wparam, lparam):
+    def on_create(self, hwnd, message, wparam, lparam):
         SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_CHILD)
         SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW)
         SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 64, 64, SWP_NOMOVE)
         ShowWindow(hwnd, SW_SHOW)
-        set_timer(5000, self.ontimer)
+        set_timer(5000, self.on_timer)
 
     @subscribe(WM_STYLECHANGED)
-    def onstyled(self, hwnd, message, wparam, lparam):
+    def on_styled(self, hwnd, message, wparam, lparam):
         if GetWindowLong(hwnd, GWL_EXSTYLE) & WS_EX_LAYERED:
             SetLayeredWindowAttributes(hwnd, 0, (255 * 70) / 100, LWA_ALPHA);
 
+    @subscribe(WM_PAINT)
+    def do_paint(self, hwnd, message, wparam, lparam):
+        pass
+
     @subscribe(WM_DESTROY)
-    def ondestroy(self, hwnd, message, wparam, lparam):
+    def on_destroy(self, hwnd, message, wparam, lparam):
         self.application.quit()
 
-    def ontimer(self, timer_id, time):
+    def on_timer(self, timer_id, time):
         kill_timer(timer_id)
         self.destroyWindow()
