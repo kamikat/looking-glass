7548709bea972781b49ef0423c55e0e56d47b9e4
kamikat <kamikat@banana.moe>
2018-03-13 21:00:42 +0800

    fix error processing message

--- lg/utils.py
+++ lg/utils.py
@@ -8,4 +8,4 @@ for k in dir(win32con):
         _MESSAGE_NAME[v] = k
 
 def get_message_name(message):
-    return _MESSAGE_NAME[message]
+    return _MESSAGE_NAME[message] if message in _MESSAGE_NAME else ("UNKNOWN(0x%x)" % message)
