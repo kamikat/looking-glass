03db9233675e63f9a9572183c26f537979729b4c
kamikat <kamikat@banana.moe>
2018-03-13 20:59:56 +0800

    move entrypoint to single script for pyinstaller

--- lg/__init__.py
+++ lg/__init__.py
@@ -1,1 +1,18 @@
+import winxpgui as win32gui
+import winapp
+
+class Application:
+
+    def __init__(self):
+        win32gui.InitCommonControls()
+        self.window = winapp.OverlayWindow()
+        self.window.prepareClass()
+        self.window.createWindow()
+
+    def start(self):
+        self.window.application = self
+        win32gui.PumpMessages()
+
+    def quit(self):
+        win32gui.PostQuitMessage(0)
 
---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+from lg import Application
+
+if __name__ == '__main__':
+    app = Application()
+    app.start()
